e6e2830933444f3df6d390be7def6d3f936d818c
soobinhand <chelsea9528@naver.com>
2021-09-06 15:02:50 +0900

    a

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app = express()
 var router = express.Router()
 var path = require('path')
 var bodyparser = require('body-parser')
+var bcrypt = require('bcrypt')
+var crypto = require('crypto')
 app.use(express.static('public'))
 app.use(bodyparser.json()) //다음에 찾아와라
 app.use(bodyparser.urlencoded({
@@ -21,6 +23,7 @@ app.post("/signup", function(req,res){
     var email = req.body.signup_email
     var id = req.body.signup_id
     var pw = req.body.signup_pw
+    
     var query = connection.query('insert into greenday_user(name,email,id,pw) values ("' + name + '","' + email + '","' + id + '","'+pw+'")',
                 function(error,rows){
                     if(error){
@@ -32,7 +35,6 @@ app.post("/signup", function(req,res){
 })
 
 const mysql = require('mysql');  // mysql 모듈 로드
-const alert = require('alert'); //alert 
 var connection = mysql.createConnection(
     {  // mysql 접속 설정
         host: 'localhost',
@@ -47,10 +49,9 @@ connection.connect();   // DB 접속
 app.post("/login",function(req,res){
     var id = req.body.login_id
     var pw = req.body.login_pw
-    
     var query = connection.query('SELECT * FROM greenday_user WHERE id = ? AND pw = ?', [id, pw],
                 function (error,rows) {
-                    console.log(rows[0].name)
+                    
                     if(rows[0]){
                         console.log("login success");
                         res.render('home.ejs',{'name':rows[0].name})
@@ -58,7 +59,6 @@ app.post("/login",function(req,res){
                     }
                     else{
                         console.log("login fail")
-                        alert('아이디 혹은 비밀번호가 틀립니다.')
                         res.redirect("/")
                     }
                     if(error){
